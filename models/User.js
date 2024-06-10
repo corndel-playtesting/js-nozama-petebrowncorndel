569712ee07837b0795641f1b0f4c4a645781fe6a
petebrowncorndel <peter.brown@corndel.com>
2024-06-10 23:14:11 +0100

    Solves day 1 exercises

--- models/User.js
+++ models/User.js
@@ -3,17 +3,17 @@ import db from '../db/index.js'
 class User {
   static async findAll() {
     const query = `
-      select id, username, firstName, lastName, email, avatar 
-      from users
+      SELECT id, username, firstName, lastName, email, avatar 
+      FROM users
     `
     const results = await db.raw(query)
     return results
   }
 
   static async findById(id) {
-    /**
-     * TODO: finish this method
-     */
+    const query = `SELECT id, username, firstName, lastName, email, avatar FROM users WHERE id = ?`
+    const results = await db.raw(query, [id])
+    return results[0]
   }
 }
 
