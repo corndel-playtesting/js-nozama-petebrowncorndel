f9d774aaeda8b843e8459f1c3977d0a1c9781ee9
petebrowncorndel <peter.brown@corndel.com>
2024-06-11 18:09:50 +0100

    Throws error if password is incorrect

--- models/User.js
+++ models/User.js
@@ -1,44 +1,53 @@
-import db from '../db/index.js'
+import db from "../db/index.js";
 
 class User {
   static async findAll() {
     const query = `
       SELECT id, username, firstName, lastName, email, avatar 
       FROM users
-    `
-    const results = await db.raw(query)
-    return results
+    `;
+    const results = await db.raw(query);
+    return results;
   }
 
   static async findById(id) {
-    const query = `SELECT id, username, firstName, lastName, email, avatar FROM users WHERE id = ?`
-    const results = await db.raw(query, [id])
-    return results[0]
+    const query = `SELECT id, username, firstName, lastName, email, avatar FROM users WHERE id = ?`;
+    const results = await db.raw(query, [id]);
+    return results[0];
   }
 
   static async create(newUser) {
     const query =
-      'INSERT INTO users (username, firstName, lastName, email, avatar, password) VALUES (?, ?, ?, ?, ?, ?) RETURNING *'
-      const results = await db.raw(query, [newUser.username, newUser.firstName, newUser.lastName, newUser.email, newUser.avatar, newUser.password])
-      return results[0]    
-    }
+      "INSERT INTO users (username, firstName, lastName, email, avatar, password) VALUES (?, ?, ?, ?, ?, ?) RETURNING *";
+    const results = await db.raw(query, [
+      newUser.username,
+      newUser.firstName,
+      newUser.lastName,
+      newUser.email,
+      newUser.avatar,
+      newUser.password,
+    ]);
+    return results[0];
+  }
 
-    static async delete(userID) {
-      const query =
-        'DELETE FROM users WHERE id = ?'
-        const results = await db.raw(query, [userID])
-        return results
-      }
+  static async delete(userID) {
+    const query = "DELETE FROM users WHERE id = ?";
+    const results = await db.raw(query, [userID]);
+    return results;
+  }
 
-      static async logIn(username, password) {
-        const query =`
+  static async logIn(username, password) {
+    const query = `
           SELECT id, username, firstName, lastName, email, avatar
           FROM users
           WHERE username = ? AND password = ?
-          `
-          const results = await db.raw(query, [username, password])
-          return results[0]
-        }
+          `;
+    const results = await db.raw(query, [username, password]);
+    if (results.length === 0) {
+      throw new Error('User not found');
+    }
+    return results[0];
   }
+}
 
-export default User
+export default User;
