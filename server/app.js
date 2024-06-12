6f59811c2dd01f84b80a9810cc7fb4e8e88ad86b
petebrowncorndel <peter.brown@corndel.com>
2024-06-12 12:30:22 +0100

    Get requests for products done, post request not working

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import Product from '../models/Product.js'
 
 const app = express()
 app.use(express.json())
@@ -9,5 +10,25 @@ app.get('/', (req, res) => {
 })
 
 // TODO: add endpoints during the workshop
+
+app.get('/products', async (req, res) => {
+  const products = await Product.findAll()
+  res.json(products)
+})
+
+app.get('/products/:productId', async (req, res) => {
+  const product = await Product.findById(req.params.productId)
+  res.json(product)
+})
+
+app.get('/products/category/:category', async (req, res) => {
+  const productsInCategory = await Product.findByCategory(req.params.category)
+  res.json(productsInCategory)
+})
+
+app.post('/products', async (req, res) => {
+  const newProduct = await Product.create(req.body.name, req.body.description, req.body.price, req.body.stockQuantity, req.body.imageURL)
+  res.json(newProduct)
+})
 
 export default app
